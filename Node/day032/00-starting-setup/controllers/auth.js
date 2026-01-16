4b8e876bc549d8ac893a6c86483c1bd2c4b7950f
Piyush Chaturvedi <67586189+PiyushChaturvedii@users.noreply.github.com>
2026-01-16 17:56:35 +0530

    cookies

---
+++ Node/day032/00-starting-setup/controllers/auth.js
@@ -0,0 +1,16 @@
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = "true";
+  const isLoggedIn =
+    req.get("Cookie").split(";")[2].trim().split("=")[1] === "true";
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true; Secure; httponly");
+  // req.isLoggedIn = true;
+  res.redirect("/");
+};
