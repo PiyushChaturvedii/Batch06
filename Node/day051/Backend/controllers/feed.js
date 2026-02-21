554e06c2235476d5b3219e73d695974d7ad9e238
Piyush Chaturvedi <67586189+PiyushChaturvedii@users.noreply.github.com>
2026-02-21 12:04:12 +0530

    imageUpload

---
+++ Node/day051/Backend/controllers/feed.js
@@ -0,0 +1,90 @@
+const { validationResult } = require("express-validator");
+
+const Post = require("../models/post");
+
+exports.getPosts = (req, res, next) => {
+  Post.find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: "Fetch posts successfully.", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+  // res.status(200).json({
+  //   posts: [
+  //     {
+  //       _id: "1",
+  //       title: "First Post",
+  //       content: "This is the first post",
+  //       imageUrl: "images/logo.png",
+  //       creator: {
+  //         name: "Ram Sharma",
+  //       },
+  //       createdAt: new Date(),
+  //     },
+  //   ],
+  // });
+};
+
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error(
+      "Validation failed, entered data is incorrect (BackEnd)",
+    );
+    error.statusCode = 422;
+    throw error;
+  }
+  if (!req.file) {
+    const error = new Error("No image provided.");
+    error.statusCode = 422;
+    throw error;
+  }
+  const imageUrl = req.file.path;
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: imageUrl,
+    creator: { name: "Ram Sharma" },
+  });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Post fetched", post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
---
+++ Node/day051/Backend/routes/feed.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { body } = require("express-validator");
+const feedcontroller = require("../controllers/feed");
+
+const router = express.Router();
+
+router.get("/posts", feedcontroller.getPosts);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedcontroller.createPost,
+);
+
+router.get("/post/:postId", feedcontroller.getPost);
+
+module.exports = router;
