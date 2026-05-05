f05a0ffae20e1a49d808635c88cb0a8d38e230c5
Piyush Chaturvedi <67586189+PiyushChaturvedii@users.noreply.github.com>
2026-05-05 11:01:09 +0530

    darkMode

---
+++ ReactJS/day019/cinemate1/cd/src/components/Footer.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+export const Footer = () => {
+  return (
+    <footer>
+
+
+<footer className="bg-neutral-primary-soft rounded-base shadow-xs border border-default m-4">
+    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span className="text-sm text-body sm:text-center">© 2023 <Link to="/" className="hover:underline">Cinemate™</Link>. All Rights Reserved.
+    </span>
+    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
+        <li>
+            <Link to="#" className="text-base hover:underline me-4 md:me-6">Instagram</Link>
+        </li>
+        <li>
+            <Link to="#" className="text-base hover:underline me-4 md:me-6">LinkedIn</Link>
+        </li>
+        <li>
+            <Link to="#" className="text-base hover:underline me-4 md:me-6">Youtube</Link>
+        </li>
+        <li>
+            <Link to="#" className="text-base hover:underline">Github</Link>
+        </li>
+    </ul>
+    </div>
+</footer>
+
+    </footer>
+  )
+}
---
+++ ReactJS/day019/cinemate1/cd/src/pages/MovieList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MovieList = () => {
+  return (
+    <main>
+      <div className="text-4xl">
+        MovieList
+      </div>
+    </main>
+  );
+};
